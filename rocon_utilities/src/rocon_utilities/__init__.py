1956e6e706b5e46242321ca69670c28ced39e5a9
hughie <hughie@hughie-base.(none)>
2013-07-19 11:44:12 +0900

    Revert "Trimmed the branch to only the rocon_hub_client sub directory"
    This reverts commit a10fd5a2ca801a08d8d4cd627d33aa58e205bfe3.

---
+++ rocon_utilities/src/rocon_utilities/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#
+# License: BSD
+#   https://raw.github.com/robotics-in-concert/rocon_multimaster/master/rocon_utilities/LICENSE
+#
+
+# Too much effort to maintain here and in the package.xml
+#__author__ = "Daniel Stonier, Jihoon Lee, Piyush Khandelwal"
+#__copyright__ = "Copyright (c) 2012 Daniel Stonier, Yujin Robot"
+#__license__ = "BSD"
+#__version__ = "0.1.0"
+#__maintainer__ = "Daniel Stonier"
+#__email__ = "d.stonier@gmail.com"
+#__date__ = "2012-11-30"
+
+import console
+from launch import main as launch, parse_rocon_launcher
+from gateways import create_gateway_remote_rule, create_gateway_rule, gateway_basename
+import ros_utilities as ros
+from ros_utilities import SubscriberProxy, find_resource
+from exceptions import TimeoutExpiredError
